bf60b90447dac077b803c6a8aa4ddb0d9490388c
Ali salhab <alisalhab258@gmail.com>
2025-05-31 12:17:58 +0300

    modules

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-// camel notation
-function sayHello() {
-  console.log("hello word");
-}
+// node module system
 
-console.log(window); //undefined
+// console.log(""); //global object
+// global.setTimeout(() => {
+//   console.log("after 2 seconds delay");
+// }, 2000);
+// setTimeout(() => {
+//   console.log("ali salhab");
+// }, 12);
 
-// node module systm
-// node package manager npm
-// building restful api with express
-// asynchronous javascript
-// callback promises async wait
-// storing data in mongodb
-// auth authorixation
-// handling and logging errors
-// unit intergration testing
-// test-driven development(tdd)
-// deploymnet to heroko
+// clearTimeout();
+// setInterval(() => {
+
+// }, 12);
+// clearInterval();
+
+// all this method are global and can be call from any file (module)
+//
+
+// var message = "";
+// console.log(global.message);
+
+// in browser all methos and variables are available in the window object
+
+// how to solve this problem ?/
+
+// this all about global object next we will intersection about node module syste m in node js
+
+var sayhello = function () {
+  console.log("hello world");
+};
+
+sayhello();
+
+// in browser we can acces it via window.sayhellow();
+// any new defintion will override any old defination with the same name
+
+// NODE MODULE SYSTEM
+
+// module
+//
