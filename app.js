2e6ac38b1ef23aa3c526a9292b3943f508a7f62a
Ali salhab <alisalhab258@gmail.com>
2025-05-31 11:59:34 +0300

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+// camel notation
+function sayHello() {
+  console.log("hello word");
+}
+
+console.log(window); //undefined
+
+// node module systm
+// node package manager npm
+// building restful api with express
+// asynchronous javascript
+// callback promises async wait
+// storing data in mongodb
+// auth authorixation
+// handling and logging errors
+// unit intergration testing
+// test-driven development(tdd)
+// deploymnet to heroko
---
+++ logger.js
@@ -0,0 +1,8 @@
+const url = "ali salhab";
+// implementation details
+function log(message) {
+  console.log(message);
+}
+
+module.exports = log;
+// module.exports.endPoint = url;
