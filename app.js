073d9ab4d2e382a103e803ac3d426b074bfdfbac
Ali salhab <alisalhab258@gmail.com>
2025-05-31 12:41:53 +0300

    module wrapper function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-// creating module
-
-// to load module we use the require keyword
-var log = require("./logger.js");
-
-console.log(log.endPoint);
+(function (exports, require, module, __filename, __dirname) {
+  console.log(__filename);
+});
+// at run time any code will be in this shape
+console.log(__filename);
+console.log(require);
+// console.log(module);
